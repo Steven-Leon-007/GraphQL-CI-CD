7345292bffb57a09f16edbdc7e26d309fd5cfd96
Steven-Leon-007 <estivlinyor@gmail.com>
2025-10-12 13:14:51 -0500

    fix: update server port to 7777 in GraphQL API

--- index.js
+++ index.js
@@ -129,6 +129,6 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true,
 }));
 
-app.listen(4000, () => {
-    console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+app.listen(7777, () => {
+    console.log('Running a GraphQL API server at http://localhost:7777/graphql');
 });
